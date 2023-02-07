ad00a9d6f2f8336f097af42daee70ba9289dd7cb
Hozackeno <floro_2002@hotmail.com>
2023-02-07 14:15:37 -0500

    Added part d

--- Lab2/Lab2-Part1-Team6.php
+++ Lab2/Lab2-Part1-Team6.php
@@ -1,5 +1,15 @@
 <head>
-    <link rel="stylesheet" href="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Art Work Database</title>
+    <link rel="stylesheet" href="./styles.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
+            $(document).ready(function() {
+                $("#clear").click(function(){
+                    $("#form").trigger("reset");
+                });
+            })
+    </script>
 </head>
 
 <body>
@@ -7,6 +17,7 @@
     <h1>Art Work Database</h1>
     <p>Description TBD</p>
 
+    <form id="form">
     <div style="float:left;">
         <label for="genre">Genre:</label>
         <select name="genre" id="genre">
@@ -43,6 +54,9 @@
     <div style="float:left;">
         <label for="museum">Museum:</label>
         <input name="museum" id="museum" type="text" class="textbox" value=""/>
+        <button type="button" id="clear">Clear Record</button>
+        <button type="button" >Save Record</button>
     </div>
+    </form>
 
 </body>
