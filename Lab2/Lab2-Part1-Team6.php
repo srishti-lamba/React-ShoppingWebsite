3197de2d0fcd491f4ffdeac42fa782ed96af45f2
Hozackeno <floro_2002@hotmail.com>
2023-02-07 17:54:07 -0500

    Created PHP Array Database

--- Lab2/Lab2-Part1-Team6.php
+++ Lab2/Lab2-Part1-Team6.php
@@ -1,65 +1,75 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Art Work Database</title>
-    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="./Lab2-Part1-Team6.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script>
-            $(document).ready(function() {
-                $("#clear").click(function(){
-                    $("#form").trigger("reset");
-                });
-            })
+        $(document).ready(function() {
+            $("#clear").click(function(){
+                $("#form").trigger("reset");
+            });
+        })
     </script>
 </head>
 
 <body>
+    <header>
+        <h1>Art Work Database</h1>
+        <p>Description TBD</p>
+    </header>
+    <?php
+        //Creates a session array database called artworks
+        require('initalizeArray.php');
+        //print_r ($_SESSION['artworks']);
+    ?>
+    <form id="form" action="./Lab2-Part1-Team6.php" method="post">
+        <div style="float:left;">
+            <label for="genre">Genre:</label>
+            <select name="genre" id="genre">
+                <option value="abstract">Abstract</option>
+                <option value="baroque">Baroque</option>
+                <option value="gothic">Gothic</option>
+                <option value="renaissance">Renaissance</option>
+            </select>
+        </div>
 
-    <h1>Art Work Database</h1>
-    <p>Description TBD</p>
+        <div class="left">
+            <label for="type">Type:</label>
+            <select name="type" id="type">
+                <optgroup label="Painting">
+                    <option value="landscape">Landscape</option>
+                    <option value="portrait">Portrait</option>
+                </optgroup>
+                <option value="sculpture">Sculpture</option>
+            </select>
+        </div>
 
-    <form id="form">
-    <div style="float:left;">
-        <label for="genre">Genre:</label>
-        <select name="genre" id="genre">
-            <option value="0">Abstract</option>
-            <option value="1">Baroque</option>
-            <option value="2">Gothic</option>
-            <option value="3">Renaissance</option>
-        </select>
-    </div>
+        <div class="left">
+            <label for="specification">Specification:</label>
+            <select name="specification" id="specification">
+                <option value="commercial">Commercial</option>
+                <option value="non-commercial">Non-commercial</option>
+                <option value="derivative-work">Derivative Work</option>
+                <option value="non-derivative-work">Non-Derivative Work</option>
+            </select>
+        </div>
 
-    <div style="float:left;">
-        <label for="type">Type:</label>
-        <select name="type" id="type">
-            <optgroup label="Painting">
-                <option value="0">Landscape</option>
-                <option value="1">Portrait</option>
-            </optgroup>
-            <option value="2">Sculpture</option>
-        </select>
-    </div>
+        <div class="left">
+            <label for="year">Year:</label>
+            <input name="year" id="year" type="number" class="textbox" value=""/>
+        </div>
 
-    <div style="float:left;">
-        <label for="specification">Specification:</label>
-        <select name="specification" id="specification">
-            <option value="0">Commercial</option>
-            <option value="1">Non-commercial</option>
-            <option value="2">Derivative Work</option>
-            <option value="3">Non-Derivative Work</option>
-        </select>
-    </div>
-
-    <div style="float:left;">
-        <label for="year">Year:</label>
-        <input name="year" id="year" type="text" class="textbox" value=""/>
-    </div>
-
-    <div style="float:left;">
-        <label for="museum">Museum:</label>
-        <input name="museum" id="museum" type="text" class="textbox" value=""/>
-        <button type="button" id="clear">Clear Record</button>
-        <button type="button" >Save Record</button>
-    </div>
+        <div class="left">
+            <label for="museum">Museum:</label>
+            <input name="museum" id="museum" type="text" class="textbox" value=""/>
+        </div>
+        <button type="button" id="clear" class="right">Clear Record</button>
+        <button class="right">Save Record</button>
     </form>
-
 </body>
+</html>
---
+++ Lab2/initalizeArray.php
@@ -0,0 +1,35 @@
+<?php
+    class artObject {
+        public $genre;
+        public $type;
+        public $specification;
+        public $year;
+        public $museum;
+    }
+    //Create array database of artObjects with empty dummy records
+    if(!isset($_SESSION['artworks'])){
+        $artworks = array();
+        $art1 = new artObject();
+        $art2 = new artObject();
+        $art3 = new artObject();
+        array_push($artworks, $art1, $art2, $art3);
+        $_SESSION['artworks'] = $artworks;
+        echo "gothere";
+    }
+    
+    //If all post parameters are set, update array database (save record)
+    if (isset($_POST['genre'], 
+              $_POST['type'], 
+              $_POST['specification'],
+              $_POST['year'],
+              $_POST['museum'])) {
+        $art = new artObject();
+        $art -> genre = $_POST['genre'];
+        $art -> type = $_POST['type'];
+        $art -> specification = $_POST['specification'];
+        $art -> year = $_POST['year'];
+        $art -> museum = $_POST['museum'];
+        array_push($_SESSION['artworks'], $art);
+    }
+    
+?>
