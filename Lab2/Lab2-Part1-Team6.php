7a0f9188e8f58872974e7b1072728cb77b8ea21e
Roberto <robertomariani258@gmail.com>
2023-02-12 15:44:20 -0500

    Added description and styling

--- Lab2/Lab2-Part1-Team6.php
+++ Lab2/Lab2-Part1-Team6.php
@@ -34,6 +34,9 @@
         $newArtwork = new artObject($_POST['genre'], $_POST['type'], $_POST['specification'], $_POST['year'], $_POST['museum']);
         array_push($_SESSION['artworks'], $newArtwork);
     } 
+
+    $artArray = $_SESSION['artworks'];
+
 ?>
 
 
@@ -42,8 +45,12 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Art Work Database</title>
-    <link rel="stylesheet" href="./Lab2-Part1-Team6.css">
+    <style>
+        <?php include 'Lab2-Part1-Team6.css';?>
+    </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+
     <script>
         $(document).ready(function() {
             $("#clear").click(function(){
@@ -64,77 +71,64 @@
 <body>
     <header>
         <h1>Art Work Database</h1>
-        <p>Description TBD</p>
+        <p>Art work database that allows you to store records of abstract, gothic, baroque and Renaissance style paintings and 
+            sculptures. The year the art was made and the museum the art can be found can also be included in the record</p>
     </header>
-    <form id="form" action="./Lab2-Part1-Team6.php" method="post">
-        <div style="float:left;">
-            <label for="genre">Genre:</label>
-            <select name="genre" id="genre" onchange="updateLiveRecord();">
-                <option value="abstract">Abstract</option>
-                <option value="baroque">Baroque</option>
-                <option value="gothic">Gothic</option>
-                <option value="renaissance">Renaissance</option>
-            </select>
+    <form class="form" id="form" action="./Lab2-Part1-Team6.php" method="post">
+        <div class="left-side-form">
+            <div>
+                <label for="genre">Genre:</label>
+                <select name="genre" id="genre" onchange="updateLiveRecord();">
+                    <option value="abstract">Abstract</option>
+                    <option value="baroque">Baroque</option>
+                    <option value="gothic">Gothic</option>
+                    <option value="renaissance">Renaissance</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="type">Type:</label>
+                <select name="type" id="type" onchange="updateLiveRecord();">
+                    <optgroup label="Painting">
+                        <option value="landscape">Landscape</option>
+                        <option value="portrait">Portrait</option>
+                    </optgroup>
+                    <option value="sculpture">Sculpture</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="specification">Specification:</label>
+                <select name="specification" id="specification" onchange="updateLiveRecord();">
+                    <option value="commercial">Commercial</option>
+                    <option value="non-commercial">Non-commercial</option>
+                    <option value="derivative-work">Derivative Work</option>
+                    <option value="non-derivative-work">Non-Derivative Work</option>
+                </select>
+            </div>
+
+            <div>
+                <label for="year">Year:</label>
+                <input name="year" id="year" type="number" class="textbox" value="" onkeyup="updateLiveRecord()"/>
+            </div>
+
+            <div>
+                <label for="museum">Museum:</label>
+                <input name="museum" id="museum" type="text" class="textbox" value="" onkeyup="updateLiveRecord()"/>
+                
+            </div>
+            <input class="form-button" type="button" id="clear" value="Clear Form" />
         </div>
 
-        <div class="left">
-            <label for="type">Type:</label>
-            <select name="type" id="type" onchange="updateLiveRecord();">
-                <optgroup label="Painting">
-                    <option value="landscape">Landscape</option>
-                    <option value="portrait">Portrait</option>
-                </optgroup>
-                <option value="sculpture">Sculpture</option>
-            </select>
+        <div class="right-side-form">
+            <input class="form-button" type="submit" name="action" value="Clear Records" />
+            <input class="form-button" type="submit" name="action" value="Save Record"/>
         </div>
-
-        <div class="left">
-            <label for="specification">Specification:</label>
-            <select name="specification" id="specification" onchange="updateLiveRecord();">
-                <option value="commercial">Commercial</option>
-                <option value="non-commercial">Non-commercial</option>
-                <option value="derivative-work">Derivative Work</option>
-                <option value="non-derivative-work">Non-Derivative Work</option>
-            </select>
-        </div>
-
-        <div class="left">
-            <label for="year">Year:</label>
-            <input name="year" id="year" type="number" class="textbox" value="" onkeyup="updateLiveRecord()"/>
-        </div>
-
-        <div class="left">
-            <label for="museum">Museum:</label>
-            <input name="museum" id="museum" type="text" class="textbox" value="" onkeyup="updateLiveRecord()"/>
-            <input type="button" id="clear" value="Clear Form" />
-        </div>
-        <input class="right" type="submit" name="action" value="Clear Records" />
-        <input class="right" type="submit" name="action" value="Save Record"/>
     </form>
 
     
-    <?php
-        $artArray = $_SESSION['artworks'];
-        if(count($artArray) > 0){
-            echo "<table><tr><th>Index</th><th>Genre</th><th>Type</th><th>Specification</th><th>Year</th><th>Museum</th></tr>";
-            for ($x = 0; $x < count($artArray); $x++) {
-                $artWork = $artArray[$x];
-                echo "<tr>";
-                echo "<td>$x</td>";
-                echo "<td>"; echo $artWork->genre; echo "</td>";
-                echo "<td>"; echo $artWork->type; echo "</td>";
-                echo "<td>"; echo $artWork->specification; echo "</td>";
-                echo "<td>"; echo $artWork->year; echo "</td>";
-                echo "<td>"; echo $artWork->museum; echo "</td>";
-                echo "</tr>";
-            }
-
-            echo "</table>";
-        }
-    ?>
-    
-    <div class="left">
-        <p>Live artwork record being formed:</p>
+    <div>
+        <h3>Based off your current inputs here is the record that will be submitted to the database:</h3>
         <table>
             <tr>
                 <th>Index</th>
@@ -154,5 +148,32 @@
             </tr>
         </table>
     </div>
+
+    <div>
+
+    <?php
+        
+        if(count($artArray) > 0){
+            echo "<h3>Saved Records:</h3>";
+            echo "<table><tr><th>Index</th><th>Genre</th><th>Type</th><th>Specification</th><th>Year</th><th>Museum</th></tr>";
+            for ($x = 0; $x < count($artArray); $x++) {
+                $artWork = $artArray[$x];
+                echo "<tr>";
+                echo "<td>$x</td>";
+                echo "<td>"; echo $artWork->genre; echo "</td>";
+                echo "<td>"; echo $artWork->type; echo "</td>";
+                echo "<td>"; echo $artWork->specification; echo "</td>";
+                echo "<td>"; echo $artWork->year; echo "</td>";
+                echo "<td>"; echo $artWork->museum; echo "</td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        }
+    ?>
+
+    </div>
+    
+    
 </body>
 </html>
