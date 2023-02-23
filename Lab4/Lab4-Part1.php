11d57c256e547d20669c5adf4b25640e5d601a1e
vanessaland <landayan.v@gmail.com>
2023-02-22 21:32:23 -0500

    added support for Microsoft Edge

--- Lab4/Lab4-Part1.php
+++ Lab4/Lab4-Part1.php
@@ -7,13 +7,15 @@
     <p> Display Cross Browsers Compatibility Issues line2</p>
 
     <?php
-        echo " Trying to detect Browser name! . <br/>";
+        echo " Trying to detect Browser name! <br/><br/>";
         function brdetect( )
         {
             $res = $_SERVER['HTTP_USER_AGENT'];
             echo $res . "<br/><br/>";
 
-            if ( strpos ($res, "Chrome") == true)
+            if ( strpos ($res, "Edg") == true)
+                echo "Browser: Microsoft Edge";
+            else if ( strpos ($res, "Chrome") == true)
                 echo "Browser: Google Chrome";
             else if ( strpos ($res, "Firefox") == true)
                 echo "Browser: Firefox";
