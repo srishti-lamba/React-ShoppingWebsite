efb40e22cce6fcfd04a47fda37ce4a82ee2dfa40
vanessaland <landayan.v@gmail.com>
2023-03-13 18:55:21 -0400

    added branch locations to About Us, updated button colours, updated contact info

--- Project_Iteration_1_2/contactus.php
+++ Project_Iteration_1_2/contactus.php
@@ -17,22 +17,31 @@
             <div class="card">
                 <img src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png">
                 <h2 class="center">Raymond Floro</h2>
-                <p class="contact center">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ Contact ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</p>
+                <p class="contact center">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ Contact ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯<br><br>
+                <a href="">raymond@smartcustomerservices.ca</a><br>
+                1-800-555-1111
+                </p>
             </div>
             <div class="card">
                 <img src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png">
                 <h2 class="center">Roberto Mariani</h2>
-                <p class="contact center">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ Contact ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</p>
+                <p class="contact center">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ Contact ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯<br><br>
+                <a href="">roberto@smartcustomerservices.ca</a><br>
+                1-800-555-2222</p>
             </div>
             <div class="card">
                 <img src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png">
                 <h2 class="center">Srishti Lamba</h2>
-                <p class="contact center">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ Contact ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</p>
+                <p class="contact center">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ Contact ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯<br><br>
+                <a href="">srishti@smartcustomerservices.ca</a><br>
+                1-800-555-3333</p>
             </div>
             <div class="card">
                 <img src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png">
                 <h2 class="center">Vanessa Landayan</h2>
-                <p class="contact center">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ Contact ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯</p>
+                <p class="contact center">⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯ Contact ⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯<br><br>
+                <a href="">vanessa@smartcustomerservices.ca</a><br>
+                1-800-555-4444</p>
             </div>
         </main>
         </div>
